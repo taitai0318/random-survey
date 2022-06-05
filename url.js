25dcb0dd80405a41fb9e1cff49b59e815d85fcff
taitai0318 <106922091+taitai0318@users.noreply.github.com>
2022-06-06 00:17:33 +0800

    Update url.js

--- url.js
+++ url.js
@@ -13,6 +13,5 @@ const urls = [
 	'https://docs.google.com/forms/d/e/1FAIpQLSd8VjlCxge6hTf1S_w0PBQY483-wYfFaee7Qcye-ZZ1XLK37g/viewform',
 	'https://docs.google.com/forms/d/e/1FAIpQLSeTLGSOqaRX-07YmloTbWkevQnWzw4vCV2OFkSd8p3W-NxiZw/viewform',
 	'https://docs.google.com/forms/d/e/1FAIpQLSezgRCRG-rX7YlqFgGfBBoSQmc_0w0V4b7aWC1_krWr_hRL_Q/viewform',
-	'https://docs.google.com/forms/d/e/1FAIpQLSfpUZUxVVh66RqdDh8e0ri39keso-227Ed9hu78qo--COzgYw/viewform',
 	'https://docs.google.com/forms/d/e/1FAIpQLSfpUZUxVVh66RqdDh8e0ri39keso-227Ed9hu78qo--COzgYw/viewform'
 ];
