cce5b46e0bd711cfcebb4b9f0d7a2052e75f7c13
taitai0318 <106922091+taitai0318@users.noreply.github.com>
2022-06-08 09:24:20 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,10 +8,10 @@
 
 
 const urls = [
-	'https://docs.google.com/forms/d/e/1FAIpQLSfz45GnuOrMHd3c0al3yBxSX5N7DimAgHQwzQK1bVp_2n4H2w/viewform',
-	'https://docs.google.com/forms/d/e/1FAIpQLSdcej70w0rW4VEqUV1Tarrllo-vRvZVXDNYQNz5_zqnj_bsNg/viewform',
-	'https://docs.google.com/forms/d/e/1FAIpQLSdALppgILneiSHn7CmgxsGJOKYUBaAFIHHtWMc5VnnZzY9uoA/viewform',
+	'https://docs.google.com/forms/d/e/1FAIpQLSf9FseoYREPLw_ooe1qQQvSyZdzQfBinsaxQR1RO4gnkG8P3w/viewform',
+	'https://docs.google.com/forms/d/e/1FAIpQLSeVTLLFTcZfRgm3DuywjFd6EaZtTJB4csPKTtEwaZq3bDIthQ/viewform',
 	'https://docs.google.com/forms/d/e/1FAIpQLScisW5w-VuWtY0NwoxhUTS_VBW37xX8i4Yf3DilKz4FArilYQ/viewform',
+	'https://docs.google.com/forms/d/e/1FAIpQLSdALppgILneiSHn7CmgxsGJOKYUBaAFIHHtWMc5VnnZzY9uoA/viewform',
 	'https://docs.google.com/forms/d/e/1FAIpQLSdcej70w0rW4VEqUV1Tarrllo-vRvZVXDNYQNz5_zqnj_bsNg/viewform',
 	'https://docs.google.com/forms/d/e/1FAIpQLSfz45GnuOrMHd3c0al3yBxSX5N7DimAgHQwzQK1bVp_2n4H2w/viewform'
 ];
